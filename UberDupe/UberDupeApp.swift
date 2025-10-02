68c03e5842ae4d6fdf451bc8dd6fb5a48c622510
rainawan <rainawan02@gmail.com>
2025-10-02 14:46:54 -0700

    Initial Commit

---
+++ UberDupe/UberDupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberDupeApp.swift
+//  UberDupe
+//
+//  Created by Raina Wan on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct UberDupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
